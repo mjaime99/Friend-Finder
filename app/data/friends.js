7bb214369616a5a4ab3ed861334cfca555c474c8
unknown <mike_jaime@live.com>
2018-10-30 22:05:59 -0700

    Start

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+var friends = [ {
+    "name" : "Josh",
+    "photo" : "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&h=350",
+    "scores" : [1,5,3,4,5,2,1,5,3,2]  
+}, {
+    "name" : "Mary",
+    "photo" : "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&h=350",
+    "scores" : [3,2,5,4,2,3,1,2,5,5]  
+}, {
+    "name" : "James",
+    "photo" : "https://image.shutterstock.com/image-photo/casually-handsome-confident-young-man-260nw-439433326.jpg",
+    "scores" : [5,4,1,2,3,5,4,2,1,3]  
+}, {
+    "name" : "Patricia",
+    "photo" : "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&h=350",
+    "scores" : [1,5,2,3,4,5,1,2,3,4]  
+}, {
+    "name" : "John",
+    "photo" : "https://img.freepik.com/fotos-gratis/homem-de-pensamento-smilin_1149-1327.jpg?size=338&ext=jpg",
+    "scores" : [1,4,5,2,3,4,5,1,2,4]  
+}, {
+    "name" : "Robert",
+    "photo" : "https://images.pexels.com/photos/373899/pexels-photo-373899.jpeg?auto=compress&cs=tinysrgb&h=350",
+    "scores" : [1,2,3,4,5,2,1,5,4,3]  
+}, {
+    "name" : "Jennifer",
+    "photo" : "https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?auto=compress&cs=tinysrgb&h=350",
+    "scores" : [3,4,5,2,1,5,4,1,4,3]  
+}, {
+    "name" : "Linda",
+    "photo" : "https://nyoobserver.files.wordpress.com/2016/03/pam-grossman-2015.jpg?quality=80&w=513",
+    "scores" : [5,4,3,1,3,5,4,1,2,5]  
+}, {
+    "name" : "Michael",
+    "photo" : "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=350",
+    "scores" : [1,3,1,2,5,4,2,3,1,5]  
+}, {
+    "name" : "Betty",
+    "photo" : "https://improvephotography.com/wp-content/uploads/2018/01/Kat-Beach-0204-Edit-2.jpg",
+    "scores" : [5,1,2,4,5,3,5,5,2,1]  
+}, {
+    "name" : "Paul",
+    "photo" : "https://media.istockphoto.com/photos/latin-man-standing-picture-id825083570?k=6&m=825083570&s=612x612&w=0&h=ITPCFaHDZb56bUTLuD5AHoxXfTYaSR-2qJFhYwIrgSg=",
+    "scores" : [1,2,3,2,1,5,5,4,2,2]
+}, {  
+    "name" : "Sandra",
+    "photo" : "https://media.istockphoto.com/photos/confident-sporty-woman-at-beach-picture-id538335514?k=6&m=538335514&s=612x612&w=0&h=ZGs6Sr4_Lm5ZZuAy3fdiyxbPPE8Z3Sa3U5ppkA2ylO8=",
+    "scores" : [1,4,5,5,3,2,5,4,2,5]  
+}];
+
+module.exports = friends;
